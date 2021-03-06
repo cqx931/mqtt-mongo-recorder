c1e2e58724408ee74652ca3eadf0ab8f47465a87
mneunomne <alberto.harres@gmail.com>
2021-03-07 00:15:15 +0100

    clean

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var mqtt     = require('mqtt');
 var config   = require('./config');
 
 const options = {
-    clientId: "ash123",
+    clientId: "ash123", // can be any name
     username: config.mqtt.user,
     password: config.mqtt.passwrd,
     debug:true
 }
-var mqttUri  = 'mqtt://' + config.mqtt.hostname //  + ':' + config.mqtt.port;
+var mqttUri  = 'mqtt://' + config.mqtt.hostname
 var client   = mqtt.connect(mqttUri, options);
 
 client.on('connect', function () {
@@ -22,7 +22,6 @@ client.on('connect', function () {
 client.on("error", function(error) { 
     console.log("Can't connect"+error);
 })
-
 
 const mongoUri = `mongodb+srv://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.hostname}/${config.mongodb.database}?retryWrites=true&w=majority`;
 
@@ -35,26 +34,22 @@ var testSchema = new mongoose.Schema({
   
 var TestModel = mongoose.model('message', testSchema);
 
-// var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
 mongoose.connect(mongoUri, { useNewUrlParser: true }, function (err, res) {
-    console.log('hello')
     if (err) {
         console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
     } else {
         console.log ('Succeeded connected to: ' + mongoUri);
     }
 
-    client.subscribe(config.mqtt.namespace,{qos:1});
-
     client.on('message',function(topic, message, packet){
+
+        console.log("message is "+ message);
+        console.log("topic is "+ topic);
 
         var newValue = new TestModel({
             name: topic,
             value: message
         })
-
-        console.log("message is "+ message);
-        console.log("topic is "+ topic);
 
         newValue.save(function (err) {
             if (err) return console.error(err);
