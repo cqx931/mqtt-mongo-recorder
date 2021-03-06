b0a8b30ffc283791195bb77e116efc88f1e48ab8
mneunomne <alberto.harres@gmail.com>
2021-03-07 00:12:43 +0100

    update project wit mongoose

--- config.js
+++ config.js
@@ -3,14 +3,18 @@ var config = {};
 config.debug = process.env.DEBUG || false;
 
 config.mqtt  = {};
-config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'localhost';
+config.mqtt.namespace = process.env.MQTT_NAMESPACE || 'dev';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '172.20.10.7';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
+config.mqtt.user      = process.env.MQTT_USER      || 'user'
+config.mqtt.password  = process.env.MQTT_PASSWORD  || ''
 
 config.mongodb = {};
-config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'localhost';
+config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'locahost';
 config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
-config.mongodb.database   = process.env.MONGODB_DATABASE   || 'mqtt';
+config.mongodb.database   = process.env.MONGODB_DATABASE   || 'db';
 config.mongodb.collection = process.env.MONGODB_COLLECTION || 'message';
+config.mongodb.user       = process.env.USER               || 'user'
+config.mongodb.password   = process.env.PASSWORD           || ''
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,41 +1,64 @@
-/**
- *
- * This NodeJS application listens to MQTT messages and records them to MongoDB
- *
- * @author  Dennis de Greef <github@link0.net>
- * @license MIT
- *
- */
-var mongodb  = require('mongodb');
+require('dotenv').config();
+
+const mongoose = require ("mongoose")
+
 var mqtt     = require('mqtt');
 var config   = require('./config');
 
-var mqttUri  = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
-var client   = mqtt.connect(mqttUri);
+const options = {
+    clientId: "ash123",
+    username: config.mqtt.user,
+    password: config.mqtt.passwrd,
+    debug:true
+}
+var mqttUri  = 'mqtt://' + config.mqtt.hostname //  + ':' + config.mqtt.port;
+var client   = mqtt.connect(mqttUri, options);
 
 client.on('connect', function () {
+    console.log("mqtt connected!");
     client.subscribe(config.mqtt.namespace);
 });
 
-var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
-mongodb.MongoClient.connect(mongoUri, function(error, database) {
-    if(error != null) {
-        throw error;
+client.on("error", function(error) { 
+    console.log("Can't connect"+error);
+})
+
+
+const mongoUri = `mongodb+srv://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.hostname}/${config.mongodb.database}?retryWrites=true&w=majority`;
+
+console.log('mongoUri', mongoUri)
+
+var testSchema = new mongoose.Schema({
+    name: String,
+    value: String
+  });
+  
+var TestModel = mongoose.model('message', testSchema);
+
+// var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+mongoose.connect(mongoUri, { useNewUrlParser: true }, function (err, res) {
+    console.log('hello')
+    if (err) {
+        console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + mongoUri);
     }
 
-    var collection = database.collection(config.mongodb.collection);
-    collection.createIndex( { "topic" : 1 } );
+    client.subscribe(config.mqtt.namespace,{qos:1});
 
-    client.on('message', function (topic, message) {
-        var messageObject = {
-            topic: topic,
-            message: message.toString()
-        };
+    client.on('message',function(topic, message, packet){
 
-        collection.insert(messageObject, function(error, result) {
-            if(error != null) {
-                console.log("ERROR: " + error);
-            }
+        var newValue = new TestModel({
+            name: topic,
+            value: message
+        })
+
+        console.log("message is "+ message);
+        console.log("topic is "+ topic);
+
+        newValue.save(function (err) {
+            if (err) return console.error(err);
+            console.log('saved to database!')
         });
     });
-});
+})
